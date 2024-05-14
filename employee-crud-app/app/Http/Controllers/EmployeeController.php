2924798d93aa31a2630f0b91cddb0348b2a9fe48
Raja Nagarajan <winrohan@gmail.com>
2024-05-14 16:35:35 +0530

    Laravel 11 CRUD Applications - Products and Employees

---
+++ employee-crud-app/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+class EmployeeController extends Controller
+{
+
+    protected $employee;
+    public function __construct(){
+
+        $this->employee = new Employee();
+        
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $response['employees'] = $this->employee->all();
+        return view('pages.index')->with($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // No need for this project
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->employee->create($request->all());
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $response['employee'] = $this->employee->find($id);
+        return view('pages.edit')->with($response);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $employee = $this->employee->find($id);
+        $employee->update(array_merge($employee->toArray(), $request->toArray()));
+        return redirect('employee');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $employee = $this->employee->find($id);
+        $employee->delete();
+        return redirect('employee');
+    }
+}
